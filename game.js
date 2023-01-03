a4bf9b489f140ec22332004a240b5baf233d4070
Luis <lusanchezmo@unal.edu.co>
2023-01-03 11:51:04 -0500

    Parte 7 del curso.

--- game.js
+++ game.js
@@ -1,1 +1,40 @@
-console.log(maps)
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+let canvasSize;
+let elementsSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize() {
+    if(window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else{
+        canvasSize = window.innerHeight * 0.8;
+    }
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+    
+    elementsSize = canvasSize / 10;
+
+    startGame();
+}
+
+function startGame() {
+    game.font = elementsSize + 'px Verdana';
+    game.textAlign = 'end';
+
+    const map = maps[0];
+    const mapRows = map.trim().split('\n');
+    const mapRowCols = mapRows.map(row => row.trim().split(''));
+
+
+    for(let i=1; i<=10; i++){
+        for(let j=1; j<=10; j++){
+            let emoji = mapRowCols[i-1][j-1];
+            game.fillText(emojis[emoji], elementsSize * j, elementsSize * i);
+        }
+    }
+}
